33816a6ce8e6bdf99da959cdddb705d9ddccb431
Ethan Vizitei <ethan.vizitei@gmail.com>
2024-07-26 11:16:35 -0500

    values to hold currently lexed tokesn

--- lexer.cpp
+++ lexer.cpp
@@ -1,3 +1,5 @@
+#include <string>
+
 enum Token {
     tok_retire = -1, // eof
     tok_propose = -2,  // define
@@ -5,3 +7,6 @@ enum Token {
     tok_title = -4,    // identifier
     tok_per_annum = -5 // number
 };
+
+static std::string TitleStr; // Filled in if tok_title
+static double PerAnnumVal;        // Filled in if tok_per_annum
