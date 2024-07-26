31a34491f2d07a40c93f46e632773d579a09aa6c
Ethan Vizitei <ethan.vizitei@gmail.com>
2024-07-26 11:20:17 -0500

    first string parsin from lexer into tokens

--- lexer.cpp
+++ lexer.cpp
@@ -10,3 +10,17 @@ enum Token {
 
 static std::string TitleStr; // Filled in if tok_title
 static double PerAnnumVal;        // Filled in if tok_per_annum
+
+static int gettoken() {
+    static int LastChar = ' ';
+    while (isspace(LastChar))
+        LastChar = getchar();
+    if (isalpha(LastChar)) {
+        TitleStr = LastChar;
+        while (isalnum((LastChar = getchar())))
+            TitleStr += LastChar;
+        if (TitleStr == "propose") return tok_propose;
+        if (TitleStr == "engage") return tok_engage;
+        return tok_title;
+    }
+}
