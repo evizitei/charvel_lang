065aecd4ad81ae0ca08128d8f56fa0aad25b9c25
Ethan Vizitei <ethan.vizitei@gmail.com>
2024-07-26 11:15:19 -0500

    propose first lexer elements

---
+++ lexer.c
@@ -0,0 +1,7 @@
+enum Token {
+    tok_retire = -1, // eof
+    tok_propose = -2,  // define
+    tok_engage = -3,   // extern
+    tok_title = -4,    // identifier
+    tok_per_annum = -5 // number
+};
